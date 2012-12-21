2bcb61d843cdceb76a8aa234f8b0cf97c5f5990e
Bhupinder <sodhi.bhupinder@gmail.com>
2012-12-21 23:41:33 +0530

    Commit from JBoss Tools

--- src/main/java/com/csaweb/servlet/EnrollUser.java
+++ src/main/java/com/csaweb/servlet/EnrollUser.java
@@ -38,11 +38,7 @@ public class EnrollUser extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		Date d = new Date();
-		
-		response.getWriter().println("Date is " + d.toString());
-		
-		response.getWriter().println("Testing datasource " + testJndiDataSource());
+		doPost(request, response);
 		
 		//response.getOutputStream().print("Date is" + d.toString());
 	}
@@ -52,7 +48,11 @@ public class EnrollUser extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+Date d = new Date();
 		
+		response.getWriter().println("Date is " + d.toString());
+		
+		response.getWriter().println("Testing datasource " + testJndiDataSource());
 		
 		response.getWriter().println("Access Token is "+request.getParameter("at"));
 		
